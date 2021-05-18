86fa0d407c3aeb14f8a48ad4194b842bc2d51d82
hitopo <919832386@qq.com>
2021-05-18 15:55:31 +0800

    微软HR面

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,66 @@
 public class Main {
     public static void main(String[] args) {
+
     }
 
-    public boolean verifyPostorder(int[] postorder) {
-        if (postorder.length < 2) {
-            return true;
+    public ListNode mergeListNodes(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
         }
-        return verifyPostorder(postorder, 0, postorder.length - 1);
+        return mergeListNodes(lists, 0, lists.length - 1);
     }
 
-    private boolean verifyPostorder(int[] postorder, int l, int r) {
-        if (l >= r) {
-            return true;
+    private ListNode mergeListNodes(ListNode[] lists, int l, int r) {
+        int len = r - l + 1;
+        if (len == 1) {
+            return lists[l];
         }
-        int rootValue = postorder[r];
-        // 找到第一个大于rootValue的节点位置k
-        int k = l;
-        while (k < r && postorder[k] < rootValue) {
-            k++;
-        }
-        for (int i = k; i < r; i++) {
-            if (postorder[i] < rootValue) {
-                return false;
+        // 划分成两半
+        int mid = l + (r - l) / 2;
+        // 左边合并
+        ListNode leftNode = mergeListNodes(lists, l, mid);
+        // 右边合并
+        ListNode rightNode = mergeListNodes(lists, mid + 1, r);
+        // 合并两个有序的链表
+        return mergeTwoListNode(leftNode, rightNode);
+    }
+
+    private ListNode mergeTwoListNode(ListNode leftNode, ListNode rightNode) {
+        ListNode dummyNode = new ListNode(-1);
+        ListNode p = dummyNode;
+        ListNode l = leftNode;
+        ListNode r = rightNode;
+        while (l != null && r != null) {
+            if (l.val < r.val) {
+                p.next = l;
+                p = l;
+                l = l.next;
+            } else {
+                p.next = r;
+                p = r;
+                r = r.next;
             }
         }
-        // 当前节点没问题，检查左右子树，左右子树中只要有一个节点失败就全部失败了
-        return verifyPostorder(postorder, l, k - 1) && verifyPostorder(postorder, k, r - 1);
+        while (l != null) {
+            p.next = l;
+            p = l;
+            l = l.next;
+        }
+        while (r != null) {
+            p.next = r;
+            p = r;
+            r = r.next;
+        }
+        return dummyNode.next;
+    }
+}
+
+class ListNode {
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
     }
 }
 
